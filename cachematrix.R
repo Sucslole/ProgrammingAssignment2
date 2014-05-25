5fd7f44245d4d08f8e780fe7f7f571eefa99cb73
Susana Lopez Leon <susana.lopez.leon@gmail.com>
2014-05-25 17:53:49 -0400

    Prova

--- cachematrix.R
+++ cachematrix.R
@@ -8,20 +8,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-makeVector <- function(x = numeric()) {
-      m <- NULL
-      set <- function(y) {
-            x <<- y
-            m <<- NULL
-      }
-      get <- function() x
-      setmean <- function(mean) 
-            m <<- mean
-      getmean <- function() m
-      list(set = set, get = get,
-           setmean = setmean,
-           getmean = getmean)
-}
 
 
 ## Write a short comment describing this function
