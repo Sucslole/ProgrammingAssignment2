835759aa0419c38cb21adb02180d01f385bc1592
Susana Lopez Leon <susana.lopez.leon@gmail.com>
2014-05-25 19:24:12 -0400

    Change

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,19 @@
 ##This function creates a special "matrix" object.
 makeCacheMatrix <- function(x = matrix()) {
       m<-NULL
-      set<-function(matrix(y,z)){
+      set<-function(matrix(y,z,z)){
             x<<-y
             m<<-NULL
       }
+      get <- function() x
+      setinverse <- function(solve) 
+      m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
             
 }
-
 
 ## Write a short comment describing this function
 
