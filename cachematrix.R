2fdbe6340746a5e15c06c1679cf41841f9413092
Susana Lopez Leon <susana.lopez.leon@gmail.com>
2014-05-25 17:41:33 -0400

    Prova

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function: 
+##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+      m<-NULL
+      set<-function(y)
 
+}
+
+makeVector <- function(x = numeric()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setmean <- function(mean) 
+            m <<- mean
+      getmean <- function() m
+      list(set = set, get = get,
+           setmean = setmean,
+           getmean = getmean)
 }
 
 
